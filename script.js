29c70f6d9727f137e4f9a6ab489abc3f522c0dc4
D. Ajay Kumar <154305661+ajay70aa@users.noreply.github.com>
2025-05-05 20:15:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const userInput = document.getElementById("text")
+const delay = document.getElementById("delay")
+const submitBtn = document.getElementById("btn")
+const display = document.getElementById("output")
+
+function wait(ms){
+	return new Promise(resolve => setTimeout(resolve, ms))
+}
+async function handleClick(){
+	await wait(Number(delay.value))
+	display.textContent = userInput.value
+}
+
+submitBtn.addEventListener('click', handleClick)
+
+
